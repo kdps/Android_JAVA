6a1c9151712d29c2bb6e29a200b4d8c6d73d3e82
kdp <kdp7584@gmail.com>
2020-04-14 21:12:02 +0900

    Create ToggleScrollListener.java

---
+++ ToggleScrollListener.java
@@ -0,0 +1,58 @@
+public abstract class ToggleScrollListener extends RecyclerView.OnScrollListener {
+    private static float HIDE_THRESHOLD = 10;
+    private int scrolledDistance = 0;
+    private boolean controlsVisible = true;
+
+    private TabLayout tabLayout;
+
+    public ToggleScrollListener (TabLayout tab) {
+        tabLayout = tab;
+    }
+
+    @Override
+    public void onScrollStateChanged (RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged (recyclerView, newState);
+
+    }
+
+    @Override
+    public void onScrolled (RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled (recyclerView, dx, dy);
+
+        FactoryLogger.Debug (scrolledDistance);
+
+        if (scrolledDistance > HIDE_THRESHOLD && controlsVisible) {
+            onHide ();
+            controlsVisible = false;
+            scrolledDistance = tabLayout.getHeight ();
+        } else if (scrolledDistance < - HIDE_THRESHOLD && !controlsVisible) {
+            onShow ();
+            controlsVisible = true;
+            scrolledDistance = - tabLayout.getHeight ();
+        }
+
+        if ((controlsVisible && dy > 0) || (! controlsVisible && dy < 0)) {
+            scrolledDistance += dy;
+        }
+    }
+
+    public abstract void onShow ();
+    public abstract void onHide ();
+
+}
+
+recyclerView.addOnScrollListener(new ToggleScrollListener (tab) {
+    @Override
+    public synchronized void onHide() {
+        if (tab != null) {
+            tab.setVisibility (View.GONE);
+        }
+    }
+
+    @Override
+    public synchronized void onShow() {
+        if (tab != null) {
+            tab.setVisibility (View.VISIBLE);
+        }
+    }
+});
