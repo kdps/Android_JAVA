ed333207a99c7e768c5393b7c7ad5339b9ed251a
kdp <kdp7584@gmail.com>
2020-04-23 20:16:02 +0900

    Update ToggleScrollListenerWithAppbar.java

--- ToggleScrollListenerWithAppbar.java
+++ ToggleScrollListenerWithAppbar.java
@@ -1,20 +1,20 @@
 public abstract class ToggleScrollListenerWithAppbar extends RecyclerView.OnScrollListener {
     private static float HIDE_THRESHOLD = 10;
-    private int scrolledDistance = 0;
     private boolean controlsVisible = true;
-
-    private TabLayout tabLayout;
+    private int scrolledDistance = 0;
+    private int appbarHeight;
+    private int inlineHeight;
+    private int headerHeight;
     private LinearLayout header;
     private AppBarLayout appBar;
-    private int appbar_height;
-    private int inline_height;
 
     public ToggleScrollListenerWithAppbar (LinearLayout _header, AppBarLayout _appBar, int height) {
         header = _header;
         appBar = _appBar;
         HIDE_THRESHOLD = height;
-        appbar_height = appBar.getMeasuredHeight ();
-        inline_height = appbar_height - height;
+        appbarHeight = appBar.getMeasuredHeight ();
+        inlineHeight = appbarHeight - height;
+        headerHeight = header.getHeight ();
     }
 
     @Override
@@ -22,52 +22,84 @@ public abstract class ToggleScrollListenerWithAppbar extends RecyclerView.OnScro
         super.onScrollStateChanged (recyclerView, newState);
     }
 
+    private void hiddenAppbar () {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams ();
+        params.height = inlineHeight;
+        appBar.setLayoutParams (params);
+    }
+
+    private void restoreAppbar () {
+        header.animate ().translationY (0).setInterpolator (new AccelerateInterpolator (0)).setDuration (0);
+
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams ();
+        params.height = appbarHeight;
+        appBar.setLayoutParams (params);
+    }
+
+    private void setStatePosition (boolean isHidden) {
+        controlsVisible = ! isHidden;
+        scrolledDistance = isHidden ? headerHeight : - headerHeight;
+    }
+
     @Override
-    public void onScrolled (RecyclerView recyclerView, int dx, int dy) {
+    public void onScrolled (final RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled (recyclerView, dx, dy);
 
-        int computeY = recyclerView.computeVerticalScrollOffset ();
+        boolean scrollToUp = (dy > 0);
+        int     computeY   = recyclerView.computeVerticalScrollOffset ();
 
         if (header != null) {
             if (computeY < HIDE_THRESHOLD) {
                 onShow ();
-                controlsVisible = true;
-                scrolledDistance = - header.getHeight ();
+                restoreAppbar ();
+                setStatePosition (false);
             } else if (scrolledDistance > HIDE_THRESHOLD && controlsVisible) {
                 onHide ();
-                controlsVisible = false;
-                scrolledDistance = header.getHeight ();
+                hiddenAppbar ();
+                setStatePosition (true);
             } else if (scrolledDistance < - HIDE_THRESHOLD && ! controlsVisible) {
                 onShow ();
-                controlsVisible = true;
-                scrolledDistance = - header.getHeight ();
+                restoreAppbar ();
+                setStatePosition (false);
             }
 
-            if (controlsVisible) {
+            if (controlsVisible && scrolledDistance < - HIDE_THRESHOLD) {
+                if (! scrollToUp) {
+                    scrolledDistance = scrolledDistance + (dy);
+                }
+
                 if (scrolledDistance > 0) {
                     header.animate ().translationY (- scrolledDistance).setInterpolator (new AccelerateInterpolator (0)).setDuration (0);
 
-                    int resizeHeight = (appbar_height - scrolledDistance);
+                    int     resizeHeight = (appbarHeight - scrolledDistance);
+                    boolean isEnough     = inlineHeight < resizeHeight;
 
-                    if (inline_height < resizeHeight) {
-                        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams ();
-                        params.height = resizeHeight;
-                        appBar.setLayoutParams (params);
-                    }
-                }
-            } else {
-                if (scrolledDistance < 0) {
-                    onShow ();
-                    int moveDistance = - (appbar_height - (- scrolledDistance));
-                    header.animate ().translationY (inline_height + moveDistance).setInterpolator (new AccelerateInterpolator (0)).setDuration (0);
-                    int resizeHeight = (inline_height + (appbar_height + moveDistance));
-
-                    if (inline_height < resizeHeight) {
+                    if (isEnough) {
                         CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams ();
                         params.height = resizeHeight;
                         appBar.setLayoutParams (params);
                     } else {
                         onHide ();
+                        hiddenAppbar ();
+                        controlsVisible = false;
+                        scrolledDistance = - headerHeight;
+                    }
+                }
+            } else {
+                if (scrollToUp && scrolledDistance > HIDE_THRESHOLD) {
+                    scrolledDistance = scrolledDistance + (dy);
+                }
+
+                if (scrolledDistance < 0) {
+                    onShow ();
+                    int moveDistance = inlineHeight + (- (appbarHeight - (- scrolledDistance)));
+                    header.animate ().translationY (moveDistance).setInterpolator (new AccelerateInterpolator (0)).setDuration (0);
+                    int resizeHeight = ((appbarHeight + moveDistance));
+
+                    if (inlineHeight < resizeHeight) {
+                        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams ();
+                        params.height = resizeHeight;
+                        appBar.setLayoutParams (params);
                     }
                 }
             }
